ccef24fc1a55942b4a820a49d80f25c59fcb8e77
graham-myles <myles.graham01@gmail.com>
2024-06-17 18:44:33 +0200

    ngmodel

---
+++ src/app/app.module.js
@@ -0,0 +1,38 @@
+// app.module.js
+
+angular.module('app', ['ngRoute', 'ngAnimate', 'ngSanitize'])
+  .config(function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
+// src/app/app.module.ts
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
+
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule, // Add FormsModule here
+    HttpClientModule,
+    RouterModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
