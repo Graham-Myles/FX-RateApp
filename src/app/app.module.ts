372807355986f515aeee6cfe330cb54374c05d3d
graham-myles <myles.graham01@gmail.com>
2024-06-17 23:33:05 +0200

    removed redundant files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; 
 
-import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
@@ -15,7 +14,6 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule, 
-    HttpClientModule,
     RouterModule
   ],
   providers: [],
